84e3a48cf0b73ab622ce9b701473a46962445c95
Okabe Dev <tanlalan123@gmail.com>
2020-04-26 17:08:14 +0700

    set up

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var express = require("express");
 var app = express();
 
+//set up socketio
+var server = require("http").Sever(app);
+const io = require("socket.io")(server)
+
+//mongodb
+
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/chatapp")
+
+app.use(express.static("public"));
 app.set("view engine" , "ejs");
-app.set(".views","./views");
+app.set("views","./views");
